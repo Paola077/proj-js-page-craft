8c3f319697e673b43b124fb1b34f91577afcbe8e
paolaperdomo <paolaperdomo079@gmail.com>
2024-10-22 09:50:28 +0200

    feat(titulo): cambiar el titulo del head, el titulo del logo y poner la primera letra en mayúscula

--- main.js
+++ main.js
@@ -8,4 +8,9 @@ const companyData = {
     "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
 };
 
-console.log(companyData);
+const newCompanyName = document.querySelector('.logo');
+newCompanyName.textContent = companyData.name;
+companyData.name[0].toUpperCase;
+
+const newTitle = document.getElementById("headTitle");
+newTitle.textContent = companyData.name;
