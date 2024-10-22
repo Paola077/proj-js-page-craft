5c89d68a6baedf528fc87041f6d6d7a6f9785721
paolaperdomo <paolaperdomo079@gmail.com>
2024-10-22 10:16:53 +0200

    feat(image): Reemplazar imagen principal, colocando la del companyData

--- main.js
+++ main.js
@@ -8,9 +8,13 @@ const companyData = {
     "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D",
 };
 
-const newCompanyName = document.querySelector('.logo');
+const newCompanyName = document.querySelector(".logo");
 newCompanyName.textContent = companyData.name;
 companyData.name[0].toUpperCase;
 
 const newTitle = document.getElementById("headTitle");
 newTitle.textContent = companyData.name;
+
+const setImage = document.getElementById("companyImage");
+setImage.setAttribute("src", "https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D");
+
